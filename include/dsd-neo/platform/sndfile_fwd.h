8df92f4beae7d80277e9ab971869d5b1e6ba326a
arancormonk <180709949+arancormonk@users.noreply.github.com>
2026-01-09 12:01:59 -0600

    platform(sndfile): forward-declare SNDFILE/SF_INFO in sndfile_fwd to fix MSVC build

--- include/dsd-neo/platform/sndfile_fwd.h
+++ include/dsd-neo/platform/sndfile_fwd.h
@@ -5,4 +5,21 @@
 
 #pragma once
 
-#include <sndfile.h>
+/**
+ * @file
+ * @brief Forward declarations for libsndfile types.
+ *
+ * Public headers should not require external library headers when only opaque
+ * pointers are needed.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct sf_private_tag SNDFILE;
+typedef struct SF_INFO SF_INFO;
+
+#ifdef __cplusplus
+}
+#endif
